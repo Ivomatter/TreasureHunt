e7b2228f37441db0655a92e40fed48f83c2167f0
unknown <ivogelezarov@gmail.com>
2024-03-16 21:24:28 +0200

    added user data script

--- server_side/scripts/server/server.py
+++ server_side/scripts/server/server.py
@@ -1,7 +1,7 @@
 import base64
 
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 from werkzeug.exceptions import RequestEntityTooLarge
 from werkzeug.datastructures import FileStorage
 
@@ -19,11 +19,8 @@ def index():
 def upload():
     try:
         file = request.files['file']
-
-        
-        app.config['uploaded_files'][file.filename] = base64.b64encode(file.read())
-        # app.config CONTAINS THE ENCODED IMAGES
-        print(app.config['uploaded_files'][file.filename])
+        if file:
+            app.config['uploaded_files'][file.filename] = base64.b64encode(file.read())
         return redirect('/')
         
   
@@ -34,12 +31,29 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+@app.route('/send-user-data', methods=['POST'])
+def submit_data():
+    data = request.json  # Extract JSON data from the request
+    user = data.get('user')
+    room = data.get('room')
+    current_riddle = data.get('current_riddle')
+    riddle_count = data.get('riddle_count')
 
+    # Do something with the data, such as storing it in a database
+    print("User:", user)
+    print("Room:", room)
+    print("Current Riddle:", current_riddle)
+    print("Riddle Count:", riddle_count)
+
+    # Respond to the client
+    response_data = {"message": "Data received successfully"}
+    return jsonify(response_data)
+    
 @app.route('/serve-image/<filename>', methods=['GET'])
 def serve_image(filename):
     if filename in app.config['uploaded_files']:
         file_contents = app.config['uploaded_files'][filename]
-        encoded_image = base64.b64encode(file_contents).decode('utf-8')
+        encoded_image = base64.b64decode(file_contents)
         return f'<img src="data:image/jpeg;base64,{encoded_image}" alt="{filename}">'
     else:
         return 'File not found.'
