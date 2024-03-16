b4e3d5f51ba119afe23b567f163f02e90fa5d75e
unknown <ivogelezarov@gmail.com>
2024-03-16 14:08:06 +0200

    Update

--- client/App.js
+++ client/App.js
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from 'react'
-// useState contains data retrieved from the backend and used to render data
-// useEffect fetch backend api on first render
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native'; // Import Text component from react-native
 
 function App() {
-  //data is a set variable, setdata is a function that manipulates data
-
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+    fetch("http://localhost:5000/members")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <div>
-    {(typeof data.members === 'undefined') ? (
-      <div>
-        Loading...
-      </div>
-    ) : (
-      data.members.map((member, i) => (
-        <p key={i}>{member}</p>
-      ))
-    )}
-     
-      </div>
-  )
+    <View>
+      {typeof data.members === 'undefined' ? (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      ) : (
+        data.members.map((member, i) => (
+          <Text key={i}>{member} AAAAAAAAAAAAAAAaTEST</Text> // Render each member inside Text component
+        ))
+      )}
+    </View>
+  );
 }
 
-export default App
+export default App; 
--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ CORS(app)
 @app.route('/members')
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
